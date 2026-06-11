065d31abf76ae3ca32fb2d472eb640ad95bc7377
loxi1 <anibal.cayetano@gmail.com>
2026-06-11 17:42:00 -0500

    change, estilos

--- apps/web-admin/src/app/layout.tsx
+++ apps/web-admin/src/app/layout.tsx
@@ -1,7 +1,18 @@
 import Providers from './providers'
 import AppShell from '@/layout/AppShell'
 import ProtectedRoute from '@/components/auth/ProtectedRoute'
+import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+})
 
 export default function RootLayout({
   children,
@@ -9,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es">
+    <html lang="es" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         <Providers>
           <ProtectedRoute>
